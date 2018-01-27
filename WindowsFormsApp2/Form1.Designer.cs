076774cb9760ed2ccf5b53c1f60148ae6bdda378
Stratos Kourtzanidis <melkor54248@yahoo.com>
2018-01-27 20:19:55 +0200

    Add first demo form

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MessageButton = new System.Windows.Forms.Button();
+            this.DisplayLabel = new System.Windows.Forms.Label();
+            this.InputTextbox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // MessageButton
+            // 
+            this.MessageButton.Enabled = false;
+            this.MessageButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.MessageButton.Location = new System.Drawing.Point(107, 142);
+            this.MessageButton.Name = "MessageButton";
+            this.MessageButton.Size = new System.Drawing.Size(75, 23);
+            this.MessageButton.TabIndex = 0;
+            this.MessageButton.Text = "Message";
+            this.MessageButton.UseVisualStyleBackColor = true;
+            this.MessageButton.Click += new System.EventHandler(this.MessageButton_Click);
+            this.MessageButton.MouseLeave += new System.EventHandler(this.MessageButton_MouseLeave);
+            this.MessageButton.MouseHover += new System.EventHandler(this.MessageButton_MouseHover);
+            // 
+            // DisplayLabel
+            // 
+            this.DisplayLabel.AutoSize = true;
+            this.DisplayLabel.Location = new System.Drawing.Point(132, 116);
+            this.DisplayLabel.Name = "DisplayLabel";
+            this.DisplayLabel.Size = new System.Drawing.Size(29, 13);
+            this.DisplayLabel.TabIndex = 1;
+            this.DisplayLabel.Text = "label";
+            // 
+            // InputTextbox
+            // 
+            this.InputTextbox.Location = new System.Drawing.Point(97, 84);
+            this.InputTextbox.Name = "InputTextbox";
+            this.InputTextbox.Size = new System.Drawing.Size(100, 20);
+            this.InputTextbox.TabIndex = 2;
+            this.InputTextbox.TextChanged += new System.EventHandler(this.InputTextbox_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(110, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Δώσε Όνομα";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "test1",
+            "test2",
+            "test3"});
+            this.comboBox1.Location = new System.Drawing.Point(97, 186);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.InputTextbox);
+            this.Controls.Add(this.DisplayLabel);
+            this.Controls.Add(this.MessageButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button MessageButton;
+        private System.Windows.Forms.Label DisplayLabel;
+        private System.Windows.Forms.TextBox InputTextbox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.DisplayLabel.Text = "";
+        }
+
+        private void MessageButton_Click(object sender, EventArgs e)
+        {
+            this.DisplayLabel.Text = "Hello " + this.InputTextbox.Text;
+            comboBox1.Items.Add("Hello " + this.InputTextbox.Text);
+        }
+
+        private void InputTextbox_TextChanged(object sender, EventArgs e)
+        {
+           // this.MessageButton.Enabled = !string.IsNullOrEmpty(this.InputTextbox.Text);
+            if (string.IsNullOrEmpty(this.InputTextbox.Text) == false)
+                this.MessageButton.Enabled = true;
+            else
+                this.MessageButton.Enabled = false;
+        }
+
+        private void MessageButton_MouseHover(object sender, EventArgs e)
+        {
+            this.Cursor = Cursors.Cross;
+        }
+
+        private void MessageButton_MouseLeave(object sender, EventArgs e)
+        {
+            this.Cursor = Cursors.Default;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            MessageBox.Show("Value changed. new value is "+comboBox1.SelectedItem );
+        }
+    }
+}
