c0c751ec53d3526c74965c48844dbf8a426a547b
Mustrahedin <33314950+Mustrahedin@users.noreply.github.com>
2018-01-27 22:51:46 +0200

    Added "Exit" button

--- WindowsFormsApp2/Form1.Designer.cs
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -33,13 +33,14 @@
             this.InputTextbox = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // MessageButton
             // 
             this.MessageButton.Enabled = false;
             this.MessageButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
-            this.MessageButton.Location = new System.Drawing.Point(107, 142);
+            this.MessageButton.Location = new System.Drawing.Point(15, 140);
             this.MessageButton.Name = "MessageButton";
             this.MessageButton.Size = new System.Drawing.Size(75, 23);
             this.MessageButton.TabIndex = 0;
@@ -52,7 +53,7 @@
             // DisplayLabel
             // 
             this.DisplayLabel.AutoSize = true;
-            this.DisplayLabel.Location = new System.Drawing.Point(132, 116);
+            this.DisplayLabel.Location = new System.Drawing.Point(12, 180);
             this.DisplayLabel.Name = "DisplayLabel";
             this.DisplayLabel.Size = new System.Drawing.Size(29, 13);
             this.DisplayLabel.TabIndex = 1;
@@ -60,7 +61,7 @@
             // 
             // InputTextbox
             // 
-            this.InputTextbox.Location = new System.Drawing.Point(97, 84);
+            this.InputTextbox.Location = new System.Drawing.Point(97, 55);
             this.InputTextbox.Name = "InputTextbox";
             this.InputTextbox.Size = new System.Drawing.Size(100, 20);
             this.InputTextbox.TabIndex = 2;
@@ -69,7 +70,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(110, 68);
+            this.label1.Location = new System.Drawing.Point(12, 58);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(72, 13);
             this.label1.TabIndex = 3;
@@ -82,17 +83,30 @@
             "test1",
             "test2",
             "test3"});
-            this.comboBox1.Location = new System.Drawing.Point(97, 186);
+            this.comboBox1.Location = new System.Drawing.Point(97, 92);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 21);
             this.comboBox1.TabIndex = 4;
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.button1.Location = new System.Drawing.Point(122, 140);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Exit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(301, 220);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.InputTextbox);
@@ -112,6 +126,7 @@
         private System.Windows.Forms.TextBox InputTextbox;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
